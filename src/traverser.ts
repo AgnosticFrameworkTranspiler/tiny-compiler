f35b4b8f432240b7a7c6fc622e1c69bbe11aaacc
roylin <1002591652@qq.com>
2020-07-12 14:28:40 +0800

    feat: added traverser

---
+++ src/traverser.ts
@@ -0,0 +1,52 @@
+import { AstNode, AstNodeType } from './parser';
+
+// 访问者
+export type Visitor = {
+  [key: string]: {
+    enter?: (node: AstNode, parent?: AstNode) => void;
+    exit?: (node: AstNode, parent?: AstNode) => void;
+  };
+};
+
+/**
+ * 遍历器
+ * @param ast 抽象语法树
+ * @param visitor 访问者
+ */
+export default function traverser(ast: AstNode, visitor: Visitor) {
+  function traverseArray(array: AstNode[], parent?: AstNode) {
+    array.forEach((child) => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node: AstNode, parent?: AstNode) {
+    const methods = visitor[node.type];
+
+    // 调用访问器入口
+    if (methods && methods.enter) {
+      methods.enter(node, parent);
+    }
+
+    switch (node.type) {
+      case AstNodeType.PROGRAM:
+        traverseArray(node.body ?? [], parent);
+        break;
+      case AstNodeType.CALL:
+        traverseArray(node.params ?? [], node);
+        break;
+      case AstNodeType.NUM:
+      case AstNodeType.STR:
+        break;
+      default:
+        throw new TypeError(node.type);
+    }
+
+    // 调用访问期器出口
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+
+    traverseNode(ast);
+  }
+}
--- tests/tokenizer.test.ts
+++ tests/tokenizer.test.ts
@@ -1,4 +1,4 @@
-import tokenizer from '../src/tokenizer';
+import tokenizer, { Token, TokenType } from '../src/tokenizer';
 
 describe(tokenizer, () => {
   it('should return an array', () => {
@@ -6,102 +6,102 @@ describe(tokenizer, () => {
   });
 
   it('should be able to tokenizer a pair of parentheses', () => {
-    const input = '()';
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Parenthesis', value: ')' },
+    const input: string = '()';
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should ignore whitespace completely', () => {
-    const input = '                  ';
-    const result: string[] = [];
+    const input: string = '                  ';
+    const result: Token[] = [];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should correctly tokenizer a single digit', () => {
-    const input = '2';
-    const result = [{ type: 'Number', value: '2' }];
+    const input: string = '2';
+    const result: Token[] = [{ type: TokenType.NUM, value: '2' }];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should be able to handle single numbers in expressions', () => {
-    const input = '(1 2)';
+    const input: string = '(1 2)';
 
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Number', value: '1' },
-      { type: 'Number', value: '2' },
-      { type: 'Parenthesis', value: ')' },
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NUM, value: '1' },
+      { type: TokenType.NUM, value: '2' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should be able to handle single letters in expressions', () => {
-    const input = '(a b)';
+    const input: string = '(a b)';
 
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Name', value: 'a' },
-      { type: 'Name', value: 'b' },
-      { type: 'Parenthesis', value: ')' },
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NAME, value: 'a' },
+      { type: TokenType.NAME, value: 'b' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should be able to handle multiple-digit numbers', () => {
-    const input = '(11 22)';
+    const input: string = '(11 22)';
 
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Number', value: '11' },
-      { type: 'Number', value: '22' },
-      { type: 'Parenthesis', value: ')' },
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NUM, value: '11' },
+      { type: TokenType.NUM, value: '22' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should correctly tokenizer a simple expression', () => {
-    const input = '(add 2 3)';
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Name', value: 'add' },
-      { type: 'Number', value: '2' },
-      { type: 'Number', value: '3' },
-      { type: 'Parenthesis', value: ')' },
+    const input: string = '(add 2 3)';
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NAME, value: 'add' },
+      { type: TokenType.NUM, value: '2' },
+      { type: TokenType.NUM, value: '3' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should ignore whitespace', () => {
-    const input = '   (add    2 3)';
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Name', value: 'add' },
-      { type: 'Number', value: '2' },
-      { type: 'Number', value: '3' },
-      { type: 'Parenthesis', value: ')' },
+    const input: string = '   (add    2 3)';
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NAME, value: 'add' },
+      { type: TokenType.NUM, value: '2' },
+      { type: TokenType.NUM, value: '3' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
   });
 
   it('should know about strings', () => {
-    const input = '(log "hello" "world")';
-    const result = [
-      { type: 'Parenthesis', value: '(' },
-      { type: 'Name', value: 'log' },
-      { type: 'String', value: 'hello' },
-      { type: 'String', value: 'world' },
-      { type: 'Parenthesis', value: ')' },
+    const input: string = '(log "hello" "world")';
+    const result: Token[] = [
+      { type: TokenType.PAREN, value: '(' },
+      { type: TokenType.NAME, value: 'log' },
+      { type: TokenType.STR, value: 'hello' },
+      { type: TokenType.STR, value: 'world' },
+      { type: TokenType.PAREN, value: ')' },
     ];
 
     expect(tokenizer(input)).toEqual(result);
